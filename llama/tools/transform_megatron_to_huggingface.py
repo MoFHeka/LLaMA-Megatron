2761d99cffebc4b5ce0ce0dea6032ee842a15392
Jia He <mofhejia@163.com>
2023-08-11 19:56:52 +0800

    [fix] Modify transfoming hf and megatron scripts.

--- llama/tools/transform_megatron_to_huggingface.py
+++ llama/tools/transform_megatron_to_huggingface.py
@@ -1,73 +1,84 @@
-import torch
 import os
+
+import torch
+from tqdm import tqdm
 
 HUG_LOAD_TEMPLATE_PATH = "HUG_LOAD_TEMPLATE_PATH"
 HUG_SAVE_PATH = "HUG_SAVE_PATH"
 MEGATRON_LOAD_PATH = "MEGATRON_LOAD_PATH"
-PP_SIZE = 2
+
+PP_SIZE = 1
 TP_SIZE = 2
-LAYER_NUM = 40
-HEAD_NUM = 40
-HIDDEN_DIM = 5120
+LAYER_NUM = 36
+HEAD_NUM = 36
+HIDDEN_DIM = 4536
+NUM_QUERY_GROUP = 2
 
 
 def prepare_v2():
     id2info = {}
-    id2info['word_embedding'] = {str(idx): {} for idx in range(TP_SIZE)}
-    id2info['transformers'] = {'layer' + str(idx): {str(tp_index): {} for tp_index in range(TP_SIZE)} for idx in
-                               range(LAYER_NUM)}
-    id2info['final_layernorm'] = {}
-    id2info['lm_head'] = {str(idx): {} for idx in range(TP_SIZE)}
+    id2info["word_embedding"] = {str(idx): {} for idx in range(TP_SIZE)}
+    id2info["transformers"] = {
+        "layer" + str(idx): {str(tp_index): {} for tp_index in range(TP_SIZE)} for idx in range(LAYER_NUM)
+    }
+    id2info["final_layernorm"] = {}
+    id2info["lm_head"] = {str(idx): {} for idx in range(TP_SIZE)}
     for tp_index in range(TP_SIZE):
         for pp_index in range(PP_SIZE):
             if not PP_SIZE or PP_SIZE == 1:
-                save_path_subfix = f'{tp_index:02d}'
+                save_path_subfix = f"{tp_index:02d}"
             else:
-                save_path_subfix = f'{tp_index:02d}_{pp_index:03d}'
-            save_path = MEGATRON_LOAD_PATH + 'mp_rank_' + save_path_subfix + '/model_optim_rng.pt'
+                save_path_subfix = f"{tp_index:02d}_{pp_index:03d}"
+            save_path = MEGATRON_LOAD_PATH + "mp_rank_" + save_path_subfix + "/model_optim_rng.pt"
             meg_state_dict = torch.load(save_path, map_location="cpu")
-            encoder = meg_state_dict['model']['language_model']['encoder']
+            encoder = meg_state_dict["model"]["language_model"]["encoder"]
             for k, v in encoder.items():
-                if 'final_layernorm' in k: continue
-                sublayer_index = int(k.split('.')[1])
+                if "final_layernorm" in k:
+                    continue
+                sublayer_index = int(k.split(".")[1])
                 layer_index = pp_index * LAYER_NUM // PP_SIZE + sublayer_index
-                para_dict = id2info['transformers']['layer' + str(layer_index)][str(tp_index)]
+                para_dict = id2info["transformers"]["layer" + str(layer_index)][str(tp_index)]
                 para_dict[k] = v
             if pp_index == 0:
-                word_embedding = meg_state_dict['model']['language_model']['embedding']['word_embeddings']['weight']
-                id2info['word_embedding'][str(tp_index)] = word_embedding
-            elif pp_index == PP_SIZE - 1:
-                lm_head = meg_state_dict['model']['language_model']['output_layer']['weight']
-                id2info['lm_head'][str(tp_index)] = lm_head
-                final_layernorm = meg_state_dict['model']['language_model']['encoder']['final_layernorm.weight']
-                id2info['final_layernorm'] = final_layernorm
+                word_embedding = meg_state_dict["model"]["language_model"]["embedding"]["word_embeddings"]["weight"]
+                id2info["word_embedding"][str(tp_index)] = word_embedding
+            if pp_index == PP_SIZE - 1:
+                lm_head = meg_state_dict["model"]["language_model"]["output_layer"]["weight"]
+                id2info["lm_head"][str(tp_index)] = lm_head
+                final_layernorm = meg_state_dict["model"]["language_model"]["encoder"]["final_layernorm.weight"]
+                id2info["final_layernorm"] = final_layernorm
     return id2info
 
 
 def load_hug_state():
     state_dict = {}
-    sub_paths = [p for p in os.listdir(HUG_LOAD_TEMPLATE_PATH) if ('pytorch' in p and 'bin' in p and '000' in p)]
-    for sub_path in sub_paths:
+    sub_paths = [p for p in os.listdir(HUG_LOAD_TEMPLATE_PATH) if ("pytorch" in p and "bin" in p and "000" in p)]
+
+    for sub_path in tqdm(sub_paths):
         p = HUG_LOAD_TEMPLATE_PATH + sub_path
-        chunk = torch.load(p, map_location='cpu')
+        chunk = torch.load(p, map_location="cpu")
         state_dict.update(chunk)
     return state_dict
 
 
 def transform_meg_to_hug(meg_state_info, hug_state_dict):
     # process last_layenorm, lm_head, embedding
+    saved_hug_state_dict = {}
     for hug_key, hug_para in hug_state_dict.items():
-        if "inv_freq" in hug_key: continue
-        if "embed_tokens" in hug_key:
+        if "inv_freq" in hug_key:
+            continue
 
+        if "embed_tokens" in hug_key:
             # hug_emb_
-            meg_paras = meg_state_info['word_embedding']
+            meg_paras = meg_state_info["word_embedding"]
             print("RAW_EMBEDDING_SIZE:\t", hug_para.size())
             meg_emb_list = [meg_paras[str(tp_index)] for tp_index in range(TP_SIZE)]
             meg_emb_concat = torch.concat(meg_emb_list, 0)
             print("MEG_EMBEDDING_SIZE:\t", meg_emb_concat.size())
-            hug_state_dict[hug_key] = meg_emb_concat
-            '''
+            saved_hug_state_dict[hug_key] = meg_emb_concat
+
+            print(hug_key, hug_para.mean(), meg_emb_concat.mean())
+            """
             hug_para = hug_para[:-256]
             pp_index = 0
             emb_chunks = torch.chunk(hug_para, TP_SIZE, 0)
@@ -77,15 +88,17 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
                 model_para_dict['embedding'] = {}
                 model_para_dict['embedding']['word_embeddings'] = {}
                 model_para_dict['embedding']['word_embeddings']['weight'] = emb_chunks[tp_index]
-            '''
-        elif 'lm_head.weight' == hug_key:
-            meg_paras = meg_state_info['lm_head']
+            """
+        elif "lm_head.weight" == hug_key:
+            meg_paras = meg_state_info["lm_head"]
             print("RAW_LM_HEAD_SIZE:\t", hug_para.size())
             meg_emb_list = [meg_paras[str(tp_index)] for tp_index in range(TP_SIZE)]
             meg_emb_concat = torch.concat(meg_emb_list, 0)
             print("MEG_LM_HEAD_SIZE:\t", meg_emb_concat.size())
-            hug_state_dict[hug_key] = meg_emb_concat
-            '''
+            saved_hug_state_dict[hug_key] = meg_emb_concat
+
+            print(hug_key, hug_para.mean(), meg_emb_concat.mean())
+            """
             pp_index = PP_SIZE - 1
             hug_para = hug_para[:-256]
             lm_head_chunks = torch.chunk(hug_para, TP_SIZE, 0)
@@ -94,29 +107,32 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
                 model_para_dict = megatron_save_info[key]['paras']['model']['language_model']
                 model_para_dict['output_layer'] = {}
                 model_para_dict['output_layer']['weight'] = lm_head_chunks[tp_index]
-            '''
+            """
 
-        elif 'model.norm.weight' == hug_key:
-            meg_final_layernorm = meg_state_info['final_layernorm']
-            hug_state_dict[hug_key] = meg_final_layernorm
-
-            '''
+        elif "model.norm.weight" == hug_key:
+            meg_final_layernorm = meg_state_info["final_layernorm"]
+            print("RAW_layernorm_SIZE:\t", hug_para.size())
+            print("MEG_layernorm_SIZE:\t", meg_final_layernorm.size())
+            saved_hug_state_dict[hug_key] = meg_final_layernorm
+            print(hug_key, hug_para.mean(), meg_final_layernorm.mean())
+            """
             pp_index = PP_SIZE - 1
             for tp_index in range(TP_SIZE):
                 key = tp_pp_2_key(tp_index, pp_index)
                 model_para_dict = megatron_save_info[key]['paras']['model']['language_model']['encoder']
                 model_para_dict['final_layernorm.weight'] = hug_para
-            '''
+            """
+
     print("FINISH PROCESS LM_HEAD/FINAL_LAYERNORM/EMBEDDING")
 
     # process attention layers
     for layer_index in range(LAYER_NUM):
         print("PROCESS ATTENTION LAYER:\t" + str(layer_index))
         hug_layer_name_prefix = "model.layers." + str(layer_index)
-        meg_paras = meg_state_info['transformers']['layer' + str(layer_index)]
+        meg_paras = meg_state_info["transformers"]["layer" + str(layer_index)]
         meg_layer_index = layer_index % (LAYER_NUM // PP_SIZE)
         # pp_index = layer_index // (LAYER_NUM // PP_SIZE)
-        '''
+        """
         q_proj = hug_state_dict[layer_prefix + '.self_attn.q_proj.weight']
         k_proj = hug_state_dict[layer_prefix + '.self_attn.k_proj.weight']
         v_proj = hug_state_dict[layer_prefix + '.self_attn.v_proj.weight']
@@ -126,23 +142,33 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
         gate_proj = hug_state_dict[layer_prefix + '.mlp.gate_proj.weight']
         input_layernorm = hug_state_dict[layer_prefix + '.input_layernorm.weight']
         post_attention_layernorm = hug_state_dict[layer_prefix + '.post_attention_layernorm.weight']
-        '''
+        """
         # recover qkv
 
         meg_qkvs = torch.concat(
-            [meg_paras[str(tp_index)]['layers.' + str(meg_layer_index) + '.self_attention.query_key_value.weight'] for
-             tp_index in range(TP_SIZE)], 0)
-        HEAD_DIM = int(meg_qkvs.size()[0] // (HEAD_NUM * 3))
+            [
+                meg_paras[str(tp_index)]["layers." + str(meg_layer_index) + ".self_attention.query_key_value.weight"]
+                for tp_index in range(TP_SIZE)
+            ],
+            0,
+        )
 
-        meg_qkvs_3d = meg_qkvs.view(HEAD_NUM, 3, HEAD_DIM, HIDDEN_DIM)
+        HEAD_DIM = int(HIDDEN_DIM // HEAD_NUM)
 
-        q, k, v = torch.chunk(meg_qkvs_3d, 3, 1)
+        print(HEAD_DIM)
 
-        print(meg_qkvs.size())
-        meg_qkvs_3d = meg_qkvs.view(HEAD_NUM, 3, HEAD_DIM, HIDDEN_DIM)
-        print(meg_qkvs_3d.size())
+        # shape = (HEAD_NUM + 2 * NUM_QUERY_GROUP) * HEAD_DIM
 
-        q, k, v = torch.chunk(meg_qkvs_3d, 3, 1)
+        # print(shape)
+
+        # print(meg_qkvs.size())
+        # meg_qkvs_3d = meg_qkvs.view(HEAD_NUM + 2 * NUM_QUERY_GROUP, 1, HEAD_DIM, HIDDEN_DIM)
+        # print(meg_qkvs_3d.size())
+
+        # q, k, v = torch.split(meg_qkvs_3d, (HEAD_NUM, NUM_QUERY_GROUP, NUM_QUERY_GROUP), dim=0)
+
+        q, k, v = tensor_split(meg_qkvs, NUM_QUERY_GROUP, HEAD_DIM)
+
         print(q.size())
         print(k.size())
         print(v.size())
@@ -152,15 +178,20 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
         # q = q.view(-1, HIDDEN_DIM)
         # k = k.view(-1, HIDDEN_DIM)
         # v = v.view(-1, HIDDEN_DIM)
-        hug_state_dict[hug_layer_name_prefix + '.self_attn.q_proj.weight'] = q
-        hug_state_dict[hug_layer_name_prefix + '.self_attn.k_proj.weight'] = k
-        hug_state_dict[hug_layer_name_prefix + '.self_attn.v_proj.weight'] = v
+        saved_hug_state_dict[hug_layer_name_prefix + ".self_attn.q_proj.weight"] = q
+        saved_hug_state_dict[hug_layer_name_prefix + ".self_attn.k_proj.weight"] = k
+        saved_hug_state_dict[hug_layer_name_prefix + ".self_attn.v_proj.weight"] = v
 
         # recover gate_up
 
         meg_gate_up = torch.concat(
-            [meg_paras[str(tp_index)]['layers.' + str(meg_layer_index) + '.mlp.dense_h_to_4h.weight'] for tp_index in
-             range(TP_SIZE)], 0)
+            [
+                meg_paras[str(tp_index)]["layers." + str(meg_layer_index) + ".mlp.dense_h_to_4h.weight"]
+                for tp_index in range(TP_SIZE)
+            ],
+            0,
+        )
+        print(meg_gate_up.size())
         meg_gate_up_3d = meg_gate_up.view(TP_SIZE, 2, -1, HIDDEN_DIM)
         print(meg_gate_up_3d.size())
 
@@ -170,30 +201,39 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
         gate = torch.reshape(gate, [-1, HIDDEN_DIM])
         print(gate.size())
         up = torch.reshape(up, [-1, HIDDEN_DIM])
-        hug_state_dict[hug_layer_name_prefix + '.mlp.gate_proj.weight'] = gate
-        hug_state_dict[hug_layer_name_prefix + '.mlp.up_proj.weight'] = up
+        saved_hug_state_dict[hug_layer_name_prefix + ".mlp.gate_proj.weight"] = gate
+        saved_hug_state_dict[hug_layer_name_prefix + ".mlp.up_proj.weight"] = up
 
         # recover layernorm
-        meg_input_layernorm = meg_paras['0']['layers.' + str(meg_layer_index) + '.input_layernorm.weight']
-        meg_post_attention_layernorm = meg_paras['0'][
-            'layers.' + str(meg_layer_index) + '.post_attention_layernorm.weight']
-        hug_state_dict[hug_layer_name_prefix + '.input_layernorm.weight'] = meg_input_layernorm
-        hug_state_dict[hug_layer_name_prefix + '.post_attention_layernorm.weight'] = meg_post_attention_layernorm
+        meg_input_layernorm = meg_paras["0"]["layers." + str(meg_layer_index) + ".input_layernorm.weight"]
+        meg_post_attention_layernorm = meg_paras["0"][
+            "layers." + str(meg_layer_index) + ".post_attention_layernorm.weight"
+        ]
+        saved_hug_state_dict[hug_layer_name_prefix + ".input_layernorm.weight"] = meg_input_layernorm
+        saved_hug_state_dict[hug_layer_name_prefix + ".post_attention_layernorm.weight"] = meg_post_attention_layernorm
 
         # recover down
 
         meg_down = torch.concat(
-            [meg_paras[str(tp_index)]['layers.' + str(meg_layer_index) + '.mlp.dense_4h_to_h.weight'] for tp_index in
-             range(TP_SIZE)], 1)
-        hug_state_dict[hug_layer_name_prefix + '.mlp.down_proj.weight'] = meg_down
+            [
+                meg_paras[str(tp_index)]["layers." + str(meg_layer_index) + ".mlp.dense_4h_to_h.weight"]
+                for tp_index in range(TP_SIZE)
+            ],
+            1,
+        )
+        saved_hug_state_dict[hug_layer_name_prefix + ".mlp.down_proj.weight"] = meg_down
 
         # recover output_proj
         meg_output = torch.concat(
-            [meg_paras[str(tp_index)]['layers.' + str(meg_layer_index) + '.self_attention.dense.weight'] for tp_index in
-             range(TP_SIZE)], 1)
-        hug_state_dict[hug_layer_name_prefix + '.self_attn.o_proj.weight'] = meg_output
+            [
+                meg_paras[str(tp_index)]["layers." + str(meg_layer_index) + ".self_attention.dense.weight"]
+                for tp_index in range(TP_SIZE)
+            ],
+            1,
+        )
+        saved_hug_state_dict[hug_layer_name_prefix + ".self_attn.o_proj.weight"] = meg_output
 
-        '''
+        """
         # qkv_chunks = torch.chunk(torch.concat([q_proj, k_proj, v_proj],0), TP_SIZE, 0)
         # q_chunks = torch.chunk(q_proj, TP_SIZE, 0)
         # k_chunks = torch.chunk(k_proj, TP_SIZE, 0)
@@ -247,17 +287,62 @@ def transform_meg_to_hug(meg_state_info, hug_state_dict):
             gate_up_proj_chunk = gate_up_chunks[tp_index]
             gate_up_chunk_key = megatron_layer_prefix + '.mlp.dense_h_to_4h.weight'
             model_para_dict[gate_up_chunk_key] = gate_up_proj_chunk
-        '''
-    return hug_state_dict
+        """
+    return saved_hug_state_dict
 
 
 def save_hug(hug_state_dict):
-    save_path = HUG_SAVE_PATH + 'pytorch_model.bin'
+    save_path = HUG_SAVE_PATH + "pytorch_model.bin"
     torch.save(hug_state_dict, save_path)
 
 
+def tensor_split(param, n_query_groups, head_size):
+    def kstart(start, blen, klen) -> int:
+        """returns start index of keys in batch"""
+        return start + (blen - (klen * 2))
+
+    def vstart(start, blen, klen) -> int:
+        """returns start index of values in batch"""
+        return start + blen - klen
+
+    def vend(start, blen) -> int:
+        """returns last index of values in batch"""
+        return start + blen
+
+    # num observations
+    nobs = param.shape[0]
+    # batch length
+    blen = nobs // n_query_groups
+    # key length in batch
+    klen = head_size
+    # value length in batch
+    vlen = head_size
+    # the starting index of each new batch
+    starts = range(0, nobs, blen)
+    # the indices to splice on
+    splices = [(s, kstart(s, blen, klen), vstart(s, blen, vlen), vend(s, blen)) for s in starts]
+
+    qc = ()
+    kc = ()
+    vc = ()
+
+    for splice in splices:
+        qs, ks, vs, ve = splice
+        qc += (param[qs:ks, :],)
+        kc += (param[ks:vs, :],)
+        vc += (param[vs:ve, :],)
+
+    q = torch.cat(qc)
+    k = torch.cat(kc)
+    v = torch.cat(vc)
+
+    return q, k, v
+
+
 megatron_load_info_plus_para = prepare_v2()
+# print(megatron_load_info_plus_para.keys())
 hug_template_state = load_hug_state()
+# print(hug_template_state.keys())
 print("FINISH LOAD HUGGING")
 hug_state_dict = transform_meg_to_hug(megatron_load_info_plus_para, hug_template_state)
 save_hug(hug_state_dict)
