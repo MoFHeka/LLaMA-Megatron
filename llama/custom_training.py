ee6fd4a89a404ce101da2a5f3bcbd5ead30ce1ec
Jia He <mofhejia@163.com>
2023-08-25 22:00:43 +0800

    [fix] Skip GPTModel check for user-custom model.

--- llama/custom_training.py
+++ llama/custom_training.py
@@ -442,9 +442,10 @@ def get_model(model_provider_func, model_type=ModelType.encoder_or_decoder, wrap
 
     # Disallow training and inference with Transformer Engine
     # for non-GPT models
-    args.allow_transformer_engine = all([type(m) == GPTModel for m in model])
-    assert args.allow_transformer_engine or args.transformer_impl == 'local', \
-        'Transformer Engine is only approved for GPT models'
+    args.allow_transformer_engine = True
+    # args.allow_transformer_engine = all([type(m) == GPTModel for m in model])
+    # assert args.allow_transformer_engine or args.transformer_impl == 'local', \
+    #     'Transformer Engine is only approved for GPT models'
 
     # Set tensor model parallel attributes if not set.
     # Only parameters that are already tensor model parallel have these
